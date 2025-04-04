ea5a3e9c818391c1c049d5c60302e8ebd58f1761
Famlonga-Nicolo-Leo <famlonga.nicolo@iisgalvanimi.edu.it>
2025-04-04 08:57:26 +0000

    continuo

--- Progetto/app.py
+++ Progetto/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
     
 app = Flask(__name__)
 
+x ="ciao"
+lista=["io","noi"]
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', paperino = x,lista= lista)
 
 if __name__ == '__main__':
     app.run(debug=True)
